ca571b7e8e61fbf0fa44f68a0058b0e1256a8c32
wanggang <wgang316@163.com>
2014-08-06 19:45:20 +0800

    Initial Commit

---
+++ ClickWebViewImage/WGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WGAppDelegate.h
+//  ClickWebViewImage
+//
+//  Created by 王刚 on 14/8/6.
+//  Copyright (c) 2014年 wwwlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClickWebViewImageTests/ClickWebViewImageTests.m
@@ -0,0 +1,34 @@
+//
+//  ClickWebViewImageTests.m
+//  ClickWebViewImageTests
+//
+//  Created by 王刚 on 14/8/6.
+//  Copyright (c) 2014年 wwwlife. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ClickWebViewImageTests : XCTestCase
+
+@end
+
+@implementation ClickWebViewImageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
